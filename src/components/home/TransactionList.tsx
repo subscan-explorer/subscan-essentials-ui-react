1e60e5c7d9e110b4a56cee9ef7605480d098e026
carlhong <acarlhong@gmail.com>
2025-05-21 16:49:32 +0800

    chore: update style

--- src/components/home/TransactionList.tsx
+++ src/components/home/TransactionList.tsx
@@ -17,7 +17,7 @@ const TransactionList: React.FC<TransactionListProps> = ({}) => {
   const extrinsicsData = unwrap(data)
   const transactions = extrinsicsData?.list
   return (
-    <div className='bg-pink-50 rounded-lg p-5'>
+    <div className='bg-blue-50 rounded-lg p-5'>
       <h2 className="text-lg font-semibold mb-4">PVM Transaction</h2>
       <Skeleton className="w-full rounded-lg" isLoaded={!isLoading}>
         <div>
